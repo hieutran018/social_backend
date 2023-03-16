443ec3d28b33a7d630788cd8017d713bf72d2729
hieutran018 <73825993+hieutran018@users.noreply.github.com>
2023-03-16 09:04:24 +0700

    update show time ago

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
         $lstPost = Post::all();
         foreach($lstPost as $post){
            $post->username = $post->user->first_name. ' ' . $post->user->last_name;
-           $post->created_at = Carbon::parse($post->created_at)->format('d:m:Y');
+           $post->created_at = Carbon::parse($post->created_at)->format('Y/m/d h:m:s');
            $post->avatarUser = URL::to('user/person/'.$post->user->id.'/'.$post->user->avatar);
         }
         return response()->json($lstPost,200);
